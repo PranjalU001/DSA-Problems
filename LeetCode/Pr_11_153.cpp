fd783d850a98564942cbbb6ca398c0c9c3c05036
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-29 23:29:26 +0530

    your commit message

--- LeetCode/Pr_11_153.cpp
+++ LeetCode/Pr_11_153.cpp
@@ -44,3 +44,10 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        return nums[0];
+    }
+};
