ff6be9745c0d6be1f47045d9ae761bc55754e747
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-30 21:43:53 +0530

    your commit message

--- LeetCode/Pr_14_15.cpp
+++ LeetCode/Pr_14_15.cpp
@@ -37,3 +37,38 @@
 
 // 3 <= nums.length <= 3000
 // -105 <= nums[i] <= 105
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> result;
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum > 0){
+                    k--;      
+                } 
+                else if(sum < 0) {
+                    j++;   
+                }
+                else {
+                    result.push_back({nums[i], nums[j], nums[k]});
+                    j++;
+                    k--;
+                    while(j < k && nums[j] == nums[j-1]) j++;   
+                }
+            }
+        }
+        return result;
+    }
+};
