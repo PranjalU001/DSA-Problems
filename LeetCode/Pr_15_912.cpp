28801a89086628698db9106043b571edcffb1452
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-30 21:45:00 +0530

    your commit message

--- LeetCode/Pr_15_912.cpp
+++ LeetCode/Pr_15_912.cpp
@@ -26,3 +26,13 @@
 
 // 1 <= nums.length <= 5 * 104
 // -5 * 104 <= nums[i] <= 5 * 104
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); 
+        return nums;
+    }
+};
