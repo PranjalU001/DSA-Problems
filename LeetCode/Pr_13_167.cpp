17244b4508c7a75552114d9db6461934601fb45d
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-30 21:43:01 +0530

    your commit message

--- LeetCode/Pr_13_167.cpp
+++ LeetCode/Pr_13_167.cpp
@@ -38,3 +38,27 @@
 // numbers is sorted in non-decreasing order.
 // -1000 <= target <= 1000
 // The tests are generated such that there is exactly one solution.
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int start = 0, end = numbers.size() - 1;
+
+        while (start < end) {
+            int sum = numbers[start] + numbers[end];
+
+            if (sum == target) {
+                return {start + 1, end + 1}; 
+            }
+            else if (sum < target) {
+                start++;
+            }
+            else {
+                end--;
+            }
+        }
+        return {};
+    }
+};
