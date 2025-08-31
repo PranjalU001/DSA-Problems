6ad7885d36d81f8a65cc85e25b9abda7d807910e
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-31 22:47:49 +0530

    your commit message

--- GFG/Pr_7.cpp
+++ GFG/Pr_7.cpp
@@ -1,1 +1,16 @@
 // Zeros count
+// User function template for C++
+
+class Solution {
+  public:
+    int countZeroes(int arr[], int n) {
+        int count=0;
+        // code here
+        for(int i=0;i<n;i++){
+            if(arr[i]==0){
+                count++;
+            }
+    }
+    return count;
+    }
+};
