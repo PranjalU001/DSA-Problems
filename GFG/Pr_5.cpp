4843f7dabc6c4a68be09689ffc29def42133461a
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-08-31 22:46:27 +0530

    your commit message

--- GFG/Pr_5.cpp
+++ GFG/Pr_5.cpp
@@ -1,1 +1,9 @@
 // Array Traversal Reverse
+class Solution {
+  public:
+    void arrayTraversalReverse(int numbers[], int size) {
+        // Code here
+        for(int i = size-1;i>=0;i--){
+        cout<<numbers[i]<<" "; }
+    }
+};
