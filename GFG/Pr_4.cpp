96a112a515e55490a137795c84aae670d363c717
PranjalU001 <upadhyaypranjal182007@gmail.com>
2025-09-13 00:24:13 +0530

    new code added

--- GFG/Pr_4.cpp
+++
@@ -1,9 +0,0 @@
-// Array Traversal Reverse
-class Solution {
-  public:
-    void arrayTraversalReverse(int numbers[], int size) {
-        // Code here
-        for(int i = size-1;i>=0;i--){
-        cout<<numbers[i]<<" "; }
-    }
-};
